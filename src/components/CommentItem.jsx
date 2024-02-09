9ef3b1cbc9bd8d01d1a4dbbe4bd337f088a638ee
Johannes Lumbantoruan <johannestrikardo@gmail.com>
2024-02-09 11:30:01 +0700

    Create comment posted time

--- src/components/CommentItem.jsx
+++ src/components/CommentItem.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import parse from 'html-react-parser';
 import CommentFooter from './CommentFooter';
+import postedAt from '../utils/postedAt';
 
 export default function CommentItem({ comment, threadId }) {
     return (
         <div className="comment-item" key={comment.id}>
-            <h3><img src={comment.owner.avatar} alt={comment.owner.name} className="avatar" />{comment.owner.name}</h3>
+            <h3 className="comment-title"><span><img src={comment.owner.avatar} alt={comment.owner.name} className="avatar" />{comment.owner.name}</span><span>{postedAt(comment.createdAt)}</span></h3>
             <div className="comment-body">{parse(comment.content)}</div>
             <CommentFooter comment={comment} threadId={threadId} />
         </div>
